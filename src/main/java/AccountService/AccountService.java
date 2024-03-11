29db18e0fc3c3014515d0c5a680c63a59350caf3
Stepan <s.ibatulov@yandex.ru>
2024-03-11 20:28:04 +0500

    2.3 "Advanced" language basics

---
+++ src/main/java/AccountService/AccountService.java
@@ -0,0 +1,24 @@
+package AccountService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountService {
+    private final Map<String, UserProfile> usersAccounts;
+    public AccountService() {
+        usersAccounts = new HashMap<>();
+    }
+
+    public void addNewUser(UserProfile userProfile) {
+        usersAccounts.put(userProfile.getLogin(), userProfile);
+    }
+
+    public void signUp(String login, String password) {
+        usersAccounts.put(login, new UserProfile(login, password));
+    }
+
+    public boolean signIn(String login, String password) {
+        UserProfile profile = usersAccounts.get(login);
+        return profile != null && profile.getLogin().equals(login) && profile.getPassword().equals(password);
+    }
+}
