663f4c6071fb581b5418fc923cafeace85218d33
Stepan <s.ibatulov@yandex.ru>
2024-04-07 12:43:34 +0500

    4.2 Tools

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,29 +1,21 @@
 package main;
 
-import dbService.DBService;
-import servlets.SignInServlet;
-import servlets.SignUpServlet;
+import chat.WebSocketChatServlet;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Подключение к БД
-        DBService dbService = new DBService();
-        dbService.printConnectInfo();
-        // Контекст
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        // Сервлеты по обработке запросов
-        context.addServlet(new ServletHolder(new SignUpServlet(dbService)), "/signup");
-        context.addServlet(new ServletHolder(new SignInServlet(dbService)), "/signin");
+        // Сервлет
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
         // Обработчик ресурсов
         ResourceHandler resourceHandler = new ResourceHandler();
         resourceHandler.setResourceBase("public_html");
